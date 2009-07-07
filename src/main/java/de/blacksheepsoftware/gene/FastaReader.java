3723a62bcaa9447ae72dbb85ab7bce197412bc1b
Bernhard Bauer <bauerb@in.tum.de>
2009-07-07 15:53:26 +0200

    Rewrote FastaReader.

--- src/main/java/de/blacksheepsoftware/gene/FastaReader.java
+++ src/main/java/de/blacksheepsoftware/gene/FastaReader.java
@@ -7,49 +7,48 @@ import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import de.blacksheepsoftware.common.LazyLinkedList;
-import de.blacksheepsoftware.common.LineIterator;
-
 /**
  * @author <a href="bauerb@in.tum.de">Bernhard Bauer</a>
  *
  */
 public class FastaReader {
 
+    protected static final String DNA_ALPHABET = "ACGT";
+    protected static final String AMINOACIDS_ALPHABET = "ACDEFGHIKLPQRSTUVWY";
+
+
     protected static final Pattern headerPattern = Pattern.compile(">\\s*(\\S+)(?:\\s+(.*))?");
 
     public static List<Sequence> sequencesInFile(BufferedReader r) throws IOException {
         final Vector<Sequence> sequences = new Vector<Sequence>();
 
-        LineIterator it = new LineIterator(r);
-        LazyLinkedList<String> l = LazyLinkedList.create(it);
-
-        while (l != null) {
-            String header = l.head();
-
-            Matcher m = headerPattern.matcher(header);
+        String line = r.readLine();
+        while (line != null) {
+            Matcher m = headerPattern.matcher(line);
             if (!m.matches()) {
-                throw new RuntimeException(); // XXX
+                throw new FileFormatException("Sequence header must start with \">\"");
             }
             final String identifier = m.group(1);
             final String description = m.group(2);
 
-            final String alphabet = "ACGT"; // XXX
+            final String alphabet;
+            if (description.endsWith("dna")) {
+                alphabet = DNA_ALPHABET;
+            } else if (description.endsWith("ami")) {
+                alphabet = AMINOACIDS_ALPHABET;
+            } else {
+                throw new FileFormatException("unknown sequence type for \""+identifier+"\"");
+            }
 
             StringBuffer content = new StringBuffer();
-
-            while ((l = l.tail()) != null) {
-                String line = l.head();
-                if (line.startsWith(">")) {
+            while (true) {
+                line = r.readLine();
+                if (line == null || line.startsWith(">")) {
                     break;
                 }
                 content.append(line);
             }
             sequences.add(new Sequence(identifier, content.toString(), alphabet));
-        }
-
-        if (it.getIOException() != null) {
-            throw it.getIOException();
         }
 
         return sequences;
