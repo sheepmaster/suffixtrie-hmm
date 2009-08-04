c27bd0b2cac8a58810f2fc35b1f671b2bfc98cc2
Bernhard Bauer <bauerb@in.tum.de>
2009-08-04 19:30:53 +0200

    Calculate average perplexity.

--- src/main/java/de/blacksheepsoftware/gene/OnlineLearningTest.java
+++ src/main/java/de/blacksheepsoftware/gene/OnlineLearningTest.java
@@ -17,8 +17,8 @@ public class OnlineLearningTest {
 
     protected static final int MAX_DEPTH = 8;
 
-    public static List<Iterable<Integer>> readAllSequences(FastaReader r) throws IOException {
-        List<Iterable<Integer>> testSequences = new ArrayList<Iterable<Integer>>();
+    public static List<List<Integer>> readAllSequences(FastaReader r) throws IOException {
+        List<List<Integer>> testSequences = new ArrayList<List<Integer>>();
         Sequence s;
         while ((s = r.readSequence()) != null) {
             testSequences.add(new IntArrayList(s, s.length()));
@@ -41,7 +41,11 @@ public class OnlineLearningTest {
             final FastaReader trainingReader = new FastaReader(new BufferedReader(new FileReader(trainingFilename)));
             final FastaReader testReader = new FastaReader(new BufferedReader(new FileReader(testFilename)));
 
-            List<Iterable<Integer>> testSequences = readAllSequences(testReader);
+            List<List<Integer>> testSequences = readAllSequences(testReader);
+            int totalLength = 0;
+            for (List<Integer> s : testSequences) {
+                totalLength += s.size();
+            }
 
             Sequence trainingSequence = trainingReader.readSequence();
 
@@ -52,20 +56,21 @@ public class OnlineLearningTest {
             Alphabet alphabet = trainingSequence.getAlphabet();
             Model model = new Model(alphabet.numberOfCharacters(), Model.Variant.PARTIAL_BACKLINKS);
 
-            System.out.println("Perplexity of new word\tPerplexity of test set");
+            System.out.println("Avg. perplexity of new word before learning\t...after learning\tAvg. perplexity of test set");
 
             while (true) {
-                double newPerplexity = model.perplexity(trainingSequence);
+                double newPerplexity = model.perplexity(trainingSequence)/trainingSequence.length();
 
                 model.learn(IntArrayList.forList(trainingSequence, trainingSequence.length()), MAX_DEPTH);
 
-                double posteriorPerplexity = model.perplexity(trainingSequence);
+                double posteriorPerplexity = model.perplexity(trainingSequence)/trainingSequence.length();
 
                 double testPerplexity = 0;
                 for (Iterable<Integer> s : testSequences) {
                     testPerplexity += model.perplexity(s);
                 }
-                System.out.println(newPerplexity+"\t"+testPerplexity+"\t"+posteriorPerplexity);
+                testPerplexity /= totalLength;
+                System.out.println(newPerplexity+"\t"+posteriorPerplexity+"\t"+testPerplexity);
 
                 trainingSequence = trainingReader.readSequence();
                 if (trainingSequence == null) {
