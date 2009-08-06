2ea04dd104fd0c21aee26b4c04ad162eaad599b8
Bernhard Bauer <bauerb@in.tum.de>
2009-08-06 12:03:11 +0200

    Added evaluation of model on a random sequence.

---
+++ src/main/java/de/blacksheepsoftware/gene/RandomSequence.java
@@ -0,0 +1,42 @@
+package de.blacksheepsoftware.gene;
+
+import java.util.Iterator;
+import java.util.Random;
+
+/**
+ * @author <a href="bauerb@in.tum.de">Bernhard Bauer</a>
+ *
+ */
+public class RandomSequence implements Iterable<Integer> {
+
+    protected final int numCharacters;
+
+    public RandomSequence(int numCharacters) {
+        this.numCharacters = numCharacters;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+
+            final Random r = new Random();
+
+            public boolean hasNext() {
+                return true;
+            }
+
+            public Integer next() {
+                return r.nextInt(numCharacters)+1;
+            }
+
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+
+        };
+    }
+
+
+}
---
+++ src/main/java/de/blacksheepsoftware/gene/RandomSequenceTest.java
@@ -0,0 +1,43 @@
+package de.blacksheepsoftware.gene;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.zip.GZIPInputStream;
+
+import de.blacksheepsoftware.hmm.Model;
+import de.blacksheepsoftware.hmm.SequenceIterator;
+
+/**
+ * @author <a href="bauerb@in.tum.de">Bernhard Bauer</a>
+ *
+ */
+public class RandomSequenceTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Usage: java "+RandomSequenceTest.class.getName()+" <HMM file>");
+            System.exit(1);
+        }
+        String hmmFileName = args[0];
+        try {
+            final Model model = (Model)new ObjectInputStream(new GZIPInputStream(new FileInputStream(hmmFileName))).readObject();
+
+            final SequenceIterator iterator = model.sequenceIterator();
+            final RandomSequence seq = new RandomSequence(model.numCharacters());
+            final double log2 = Math.log(2);
+
+            for (int c : seq) {
+                System.out.println(iterator.score(c) / log2);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
