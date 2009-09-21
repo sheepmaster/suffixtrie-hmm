d206ce5c836d25033f7bcbdcf7a28f937ac555ad
Bernhard Bauer <bauerb@in.tum.de>
2009-09-21 18:14:06 +0200

    tmp

--- src/main/java/de/tum/in/lrr/hmm/gene/SubSequenceSearch.java
+++ src/main/java/de/tum/in/lrr/hmm/gene/SubSequenceSearch.java
@@ -1,69 +1,115 @@
 package de.tum.in.lrr.hmm.gene;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.PriorityQueue;
+import java.util.List;
 
 import de.tum.in.lrr.hmm.ISequence;
 import de.tum.in.lrr.hmm.SequenceIterable;
+import de.tum.in.lrr.hmm.util.GeneratingIterator;
 
 /**
  * @author <a href="bauerb@in.tum.de">Bernhard Bauer</a>
  *
  */
-public class SubSequenceSearch implements Iterator<ScoredSequence> {
+public class SubSequenceSearch extends GeneratingIterator<ScoredSequence> {
 
-    protected final ISequence sequence;
+    final ISequence sequence;
 
-    protected final SequenceIterable model;
-    protected final SequenceIterable baseModel;
+    final Iterator<Double> modelIterator;
+    final Iterator<Double> baseModelIterator;
+
+    final List<ScoredSequence> subSequences = new ArrayList<ScoredSequence>();
+    final List<Double> startTotals = new ArrayList<Double>();
+    final List<Double> endTotals = new ArrayList<Double>();
+
+    double total = 0;
+    int index = 0;
+
 
     public SubSequenceSearch(SequenceIterable model, SequenceIterable baseModel, ISequence sequence) {
         this.sequence = sequence;
-        this.model = model;
-        this.baseModel = baseModel;
-        addSearchHit(sequence);
-    }
+        final Iterator<Byte> iterator1 = sequence.iterator();
+        final Iterator<Byte> iterator2 = sequence.iterator();
+        modelIterator = model.scoringIterator(iterator1);
+        baseModelIterator = baseModel.scoringIterator(iterator2);
 
-
-    protected PriorityQueue<ScoredSequence> searchHitQueue = new PriorityQueue<ScoredSequence>(11, Collections.reverseOrder());
-
-    /**
-     * {@inheritDoc}
-     */
-    public boolean hasNext() {
-        return !searchHitQueue.isEmpty();
     }
 
     /**
      * {@inheritDoc}
      */
-    public ScoredSequence next() {
-        final ScoredSequence s = searchHitQueue.remove();
-        addSearchHit(s.precedingSubSequence());
-        addSearchHit(s.followingSubSequence());
-        return s;
+    @Override
+    protected boolean canGenerateValues() {
+        return modelIterator.hasNext();
     }
 
     /**
-     * @param seq
+     * 
      */
-    private void addSearchHit(final ISequence seq) {
-        if (seq.length() == 0) {
-            return;
+    private int findIndex(double startTotal) {
+        for (int i = subSequences.size()-1; i >= 0; i++) {
+            if (startTotals.get(i) < total) {
+                return i;
+            }
         }
-        final ScoredSequence hit = ScoredSequence.search(model, baseModel, seq);
-        if (hit.length() > 0) {
-            searchHitQueue.add(hit);
-        }
+        return -1;
+    }
 
+    /**
+     * @param start
+     * @param end
+     * @param startTotal
+     * @param endTotal
+     */
+    private void addSequence(int start, int end, double startTotal, double endTotal) {
+        final ScoredSequence seq = new ScoredSequence(sequence, start, end, endTotal-startTotal);
+        subSequences.add(seq);
+        startTotals.add(startTotal);
+        endTotals.add(endTotal);
+    }
+
+    void addOrMergeSequence(int start, int end, double startTotal, double endTotal) {
+        final int j = findIndex(startTotal);
+
+        if (j == -1) {
+            addSubSequences();
+            addSequence(start, end, startTotal, endTotal);
+        } else if (endTotals.get(j) >= endTotal) {
+            addSequence(start, end, startTotal, endTotal);
+        } else {
+            final ScoredSequence seq_j = subSequences.get(j);
+            subSequences.subList(j, subSequences.size()).clear();
+            addOrMergeSequence(seq_j.getStartIndex(), end, startTotals.get(j), endTotal);
+        }
+    }
+
+    /**
+     * 
+     */
+    private void addSubSequences() {
+        for (ScoredSequence s : subSequences) {
+            addValue(s);
+        }
+        subSequences.clear();
     }
 
     /**
      * {@inheritDoc}
      */
-    public void remove() {
-        throw new UnsupportedOperationException();
+    @Override
+    protected void generateValues() {
+        final double score = baseModelIterator.next() - modelIterator.next();
+
+        final double newTotal = score+total;
+        if (score > 0) {
+            addOrMergeSequence(index, index+1, total, newTotal);
+        }
+        total = newTotal;
+
+        if (!modelIterator.hasNext()) {
+            addSubSequences();
+        }
     }
 
 
